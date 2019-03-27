e77f56b76f269891ddaf2c118943d817fcb3627f
masc-potato <rpossignolo@gmail.com>
2019-03-26 18:48:27 -0700

    Add files via upload

--- resources/todomvc/vanilla-examples/es2015/README.md
+++ resources/todomvc/vanilla-examples/es2015/README.md
@@ -1,4 +1,4 @@
-# Speedometer 2.0: Vanilla JavaScript TodoMVC example
+# Speedometer 2.0: ES2015 TodoMVC example
 
 ## Test locally
 
--- resources/todomvc/vanilla-examples/es2015/src/model.js
+++ resources/todomvc/vanilla-examples/es2015/src/model.js
@@ -1,13 +1,13 @@
-(function (window) {
-    'use strict';
+'use strict';
 
-    /**
-     * Creates a new Model instance and hooks up the storage.
-     *
-     * @constructor
-     * @param {object} storage A reference to the client side storage class
-     */
-    function Model(storage) {
+
+/**
+ * Creates a new Model instance and hooks up the storage.
+ * @constructor
+ * @param {object} storage A reference to the client side storage class
+ */
+class Model {
+    constructor(storage) {
         this.storage = storage;
     }
 
@@ -17,47 +17,45 @@
      * @param {string} [title] The title of the task
      * @param {function} [callback] The callback to fire after the model is created
      */
-    Model.prototype.create = function (title, callback) {
+    create(title, callback){
         title = title || '';
-        callback = callback || function () {};
 
-        var newItem = {
+        let newItem = {
             title: title.trim(),
             completed: false
         };
 
         this.storage.save(newItem, callback);
-    };
+    }
 
     /**
      * Finds and returns a model in storage. If no query is given it'll simply
      * return everything. If you pass in a string or number it'll look that up as
-     * the ID of the model to find. Lastly, you can pass it an object to match
+     * the ID of the model to find. Lastly, you can pass it an object to match
      * against.
      *
      * @param {string|number|object} [query] A query to match models against
      * @param {function} [callback] The callback to fire after the model is found
      *
      * @example
-     * model.read(1, func); // Will find the model with an ID of 1
-     * model.read('1'); // Same as above
+     * model.read(1, func) // Will find the model with an ID of 1
+     * model.read('1') // Same as above
      * //Below will find a model with foo equalling bar and hello equalling world.
-     * model.read({ foo: 'bar', hello: 'world' });
+     * model.read({ foo: 'bar', hello: 'world' })
      */
-    Model.prototype.read = function (query, callback) {
-        var queryType = typeof query;
-        callback = callback || function () {};
+    read(query, callback){
+        const queryType = typeof query;
 
         if (queryType === 'function') {
             callback = query;
-            return this.storage.findAll(callback);
+            this.storage.findAll(callback);
         } else if (queryType === 'string' || queryType === 'number') {
             query = parseInt(query, 10);
-            this.storage.find({ id: query }, callback);
+            this.storage.find({id: query}, callback);
         } else {
             this.storage.find(query, callback);
         }
-    };
+    }
 
     /**
      * Updates a model by giving it an ID, data to update, and a callback to fire when
@@ -67,9 +65,9 @@
      * @param {object} data The properties to update and their new value
      * @param {function} callback The callback to fire when the update is complete.
      */
-    Model.prototype.update = function (id, data, callback) {
+    update(id, data, callback){
         this.storage.save(data, callback, id);
-    };
+    }
 
     /**
      * Removes a model from storage
@@ -77,31 +75,31 @@
      * @param {number} id The ID of the model to remove
      * @param {function} callback The callback to fire when the removal is complete.
      */
-    Model.prototype.remove = function (id, callback) {
+    remove(id, callback){
         this.storage.remove(id, callback);
-    };
+    }
 
     /**
      * WARNING: Will remove ALL data from storage.
      *
      * @param {function} callback The callback to fire when the storage is wiped.
      */
-    Model.prototype.removeAll = function (callback) {
+    removeAll(callback){
         this.storage.drop(callback);
-    };
+    }
 
     /**
      * Returns a count of all todos
      */
-    Model.prototype.getCount = function (callback) {
-        var todos = {
+    getCount(callback){
+        let todos = {
             active: 0,
             completed: 0,
             total: 0
         };
 
-        this.storage.findAll(function (data) {
-            data.forEach(function (todo) {
+        this.storage.findAll(data => {
+            for (let todo of data) {
                 if (todo.completed) {
                     todos.completed++;
                 } else {
@@ -109,12 +107,11 @@
                 }
 
                 todos.total++;
-            });
-            callback(todos);
-        });
-    };
+            }
 
-    // Export to window
-    window.app = window.app || {};
-    window.app.Model = Model;
-})(window);
+            if (callback) {
+                callback(todos);
+            }
+        });
+    }
+}
